f0367af6d47e5409e1ef0ba7c012d1211881d573
ClarusIubar <iubar@duck.com>
2026-01-15 23:08:09 +0900

    threadingHTTPServer

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from http.server import HTTPServer
+from http.server import ThreadingHTTPServer
 from handler import EssentialHandler
 from session import SessionStore
 from functools import partial
 
+HOST, PORT = 'localhost', 8000
+
 if __name__ == "__main__":
     store = SessionStore()
     handler_factory = partial(EssentialHandler, store=store)
-    server = HTTPServer(('localhost', 8000), handler_factory)
-    server.serve_forever()
+    server = ThreadingHTTPServer((HOST, PORT), handler_factory)
+    
+    print(f"병렬 세션 시스템 기동: http://{HOST}:{PORT}")
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\n자원 정리 및 종료")
+        server.server_close()
