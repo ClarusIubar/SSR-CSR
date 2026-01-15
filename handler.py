dc16755d3ccd43b66fb6ad9bda941595c3bd8bf0
ClarusIubar <iubar@duck.com>
2026-01-15 22:26:01 +0900

    my style

--- handler.py
+++ handler.py
@@ -3,56 +3,57 @@ import uuid
 import os
 from http.server import BaseHTTPRequestHandler
 
-# 1. 데이터 저장소
 db = {}
 
 class EssentialHandler(BaseHTTPRequestHandler):
-    
+
     def _read_file(self, name):
         if not os.path.exists(name):
             return ""
-        with open(name, 'r', encoding='utf-8') as f:
-            return f.read()
-
-    def _get_field(self, params, key):
-        values = params.get(key)
-        if values:
-            return values[0].strip()
-        else:
-            return ""
-
-    def _render_full_page(self):
+        with open(name, "r", encoding='utf-8') as file:
+            return file.read()
+    
+    def _get_field(self, data, key):
+        value = data.get(key)
+        if value:
+            return value[0].strip()
+        return ""
+    
+    def _render(self):
         layout = self._read_file('index.html')
-        
+
         if not db:
-            content = self._read_file('empty.html')
+            rows = self._read_file('empty.html')
         else:
             row = self._read_file('row.html')
-            content = "".join(row.format(uid=u, task=i['task']) for u, i in db.items())
+            rows = "".join(row.format(uid=u, task=i['task']) for u, i in db.items())
 
-        return layout.replace('{rows}', content).encode('utf-8')
-
+        return layout.replace('{rows}', rows).encode('utf-8')
+    
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
             self.send_header('Content-type', 'text/html; charset=utf-8')
             self.end_headers()
-            self.wfile.write(self._render_full_page())
+            self.wfile.write(self._render())
 
     def do_POST(self):
-        length = int(self.headers.get('Content-Length', 0))
-        params = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-        
-        id = self._get_field(params, 'id')
-        task = self._get_field(params, 'task')
+        size = int(self.headers.get('Content-Length', 0))
+        body = self.rfile.read(size).decode('utf-8')
+
+        data = urllib.parse.parse_qs(body)
+
+        uid = self._get_field(data, 'id')
+        task = self._get_field(data, 'task')
 
         if self.path == '/delete':
-            db.pop(id, None)
+            db.pop(uid, None)
+
         elif self.path == '/process':
-            if id in db:
-                db[id] = {'task': task}
+            if uid in db:
+                db[uid] = {'task' : task}
             else:
-                db[str(uuid.uuid4())] = {'task': task}
+                db[str(uuid.uuid4())] = {'task' : task}
 
         self.send_response(303)
         self.send_header('Location', '/')
