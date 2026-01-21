c323e6e87fe800dbd9b3846277cbd07cb49cf4b3
ClarusIubar <iubar@duck.com>
2026-01-21 20:54:42 +0900

    observer.py: adopt observer pattern

---
+++ observer.py
@@ -0,0 +1,18 @@
+# 뭔 시퀀스 핸들링이랑 오케스트레이션이여 지금은 사치야.
+# 그냥 뭔 일이 일어나는지나 파악합시다.
+
+import json
+
+class Observer:
+    # 옵저버 부모임.
+
+class CLIObserver(Observer):
+    # 터미널에 보여주는 용도임.
+
+class BrowserObserver(Observer):
+    # 브라우저 콘솔에 보여주는 용도임.
+
+class Subject:
+    # 아 몰라, 서브젝트가 주체래. 옵저버 패턴에 필요한가벼.
+    # 나는 날라리여. 정해주면 AI가 채워주겠지.
+    # 코드짜는 거 구경할려.
