0acc5a2d156683a30b4436e58b641e851a99ded8
ClarusIubar <iubar@duck.com>
2026-01-21 20:59:35 +0900

    logic.py: CRUD

---
+++ logic.py
@@ -0,0 +1,19 @@
+# 로직이 일어날 때, 알려줘야 된대. 그럼 로직은 따로 둬야겠지.
+# 보고 하세요. 일꾼이여.
+
+# 일할거면, A4용지는 있어야겠지.
+storage = [] # 배열을 싫어하지만, sequence적인 자료에는 주효하니까 초기화 하자.
+monitor = None # 어떻게든 하면, 밖에 알려주겠지.
+
+# do? act? CRUD? perform!
+def perform_create(*args):
+    return NotImplemented
+
+def perform_read(*args):
+    return NotImplemented
+
+def perform_update(*args):
+    return NotImplemented
+
+def perform_delete(*args):
+    return NotImplemented
