4883b491ea871386eeef1cc3891a9ccb024f8dc5
유효현 <clarusiubar[@gmail.com](mailto:you@example.com)>
2026-01-22 12:22:24 +0900

    main.py: exit(0)추가

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from http.server import HTTPServer
 from handler import Handler
 
@@ -16,3 +16,5 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n종료")
         server.server_close()
+        sys.exit(0) # 정찰자를 넣어서 나의 신호가 씹히나보다.
+        # 그렇다고 거기를 또 손대면 안됨.
